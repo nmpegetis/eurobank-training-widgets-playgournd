04976aca2b0424b7240e3c1a0d6d022ad106b82d
Nikolas Begetis <nmpegetis@gmail.com>
2019-06-10 15:55:33 +0300

    feat: add some addons(actions,knobs,notes) to storybook

--- stories/index.js
+++ stories/index.js
@@ -1,11 +1,39 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { Button } from '@storybook/react/demo';
+import { action } from '@storybook/addon-actions';
+import { withKnobs, text, boolean, object } from '@storybook/addon-knobs';
 
-storiesOf('Button', module).add('with text', () => <Button>Hello Button</Button>).add('with emoji', () => (
-	<Button>
-		<span role="img" aria-label="so cool">
-			😀 😎 👍 💯
-		</span>
-	</Button>
-));
+storiesOf('Button', module)
+	.addDecorator(withKnobs)
+	.add(
+		'with text',
+		() => {
+			const style = {
+				backgroundColor: '#FFF',
+				border: '1px solid purple',
+				borderRadius: 2,
+				outline: 0,
+				fontSize: 15,
+				cursor: 'pointer',
+			};
+			const name = text('Name', 'Click me!');
+
+			return (
+				<button
+					disabled={boolean('Disabled', false)}
+					style={object('style', style)}
+					onClick={action('clicked')}
+				>
+					{name}
+				</button>
+			);
+		},
+		{ notes: 'A very simple button component' }
+	)
+	.add('with emoji', () => (
+		<button>
+			<span role="img" aria-label="so cool">
+				😀 😎 👍 💯
+			</span>
+		</button>
+	));

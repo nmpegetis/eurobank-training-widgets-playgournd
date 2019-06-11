1c0f489b14dc65070e3191eaf57500623a3203f0
Nikolas Begetis <nmpegetis@gmail.com>
2019-06-12 01:07:04 +0300

    feat: refactor Table with projectStatus's data from Ant Design (workshop solution)

--- stories/index.js
+++ stories/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
-import { withKnobs, date, text, boolean, select } from '@storybook/addon-knobs';
+import { withKnobs, date, text, boolean, select, object } from '@storybook/addon-knobs';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import InputLabel from '@material-ui/core/InputLabel';
@@ -121,138 +121,89 @@ storiesOf('Board', module).addDecorator(withKnobs).add('Table', () => {
 		((d) => new Date(d.setDate(d.getDate() - days)))(new Date()).toLocaleDateString();
 	const chooseDaysAfter = (days) => ((d) => new Date(d.setDate(d.getDate() + days)))(new Date()).toLocaleDateString();
 
-	// TODO: For use in the exercise below
-	// const unit = [ 'Centralized Services & Digital Branches' ];
-	// const pem = [ '10000', '10001', '10010', '10011' ];
-	// const title = [ 'Lifemoments', 'PricingTool', 'LendingTool', 'WizardTool' ];
-	// const desc = [ 'test', 'test2', 'test3' ];
-	// const supervisor = [ 'Super1', 'Super2', 'Super3' ];
-	// const team = [
-	// 	[ 'Member1', 'Member2', 'Member3' ],
-	// 	[ 'Member4', 'Member5', 'Member6', 'Member7' ],
-	// 	[ 'Member8', 'Member9' ],
-	// 	[ 'Member10' ],
-	// ];
-	// const startingDate = [ chooseDaysBefore(10), chooseDaysBefore(32), chooseDaysBefore(15), chooseDaysBefore(45) ];
-	// const endingDate = [ chooseDaysAfter(40), chooseDaysAfter(60), chooseDaysAfter(50), chooseDaysAfter(4) ];
-	// const comments = [
-	// 	'nothing to mention',
-	// 	'low team capacity due to holiday season',
-	// 	'many experienced developers in team',
-	// ];
-	// const mandays = [ 100, 140, 150 ];
-	// const lastUpdate = [ chooseDaysBefore(3), chooseDaysBefore(1), chooseDaysBefore(0), , chooseDaysBefore(6) ];
-	// const status = [ 'Started', 'On going', 'Finishing' ];
-
-	// const projectStatusData = () => ({
-	// 	unit: unit[Math.floor(Math.random() * unit.length)],
-	// 	pem: pem[Math.floor(Math.random() * pem.length)],
-	// 	title: title[Math.floor(Math.random() * title.length)],
-	// 	desc: desc[Math.floor(Math.random() * desc.length)],
-	// 	supervisor: supervisor[Math.floor(Math.random() * supervisor.length)],
-	// 	team: team[Math.floor(Math.random() * team.length)],
-	// 	startingDate: startingDate[Math.floor(Math.random() * startingDate.length)],
-	// 	endingDate: endingDate[Math.floor(Math.random() * endingDate.length)],
-	// 	comments: comments[Math.floor(Math.random() * comments.length)],
-	// 	mandays: mandays[Math.floor(Math.random() * mandays.length)],
-	// 	lastUpdate: lastUpdate[Math.floor(Math.random() * lastUpdate.length)],
-	// 	status: status[Math.floor(Math.random() * status.length)],
-	// });
-
-	// TODO: Create an array of four(4) elements filled in with projectStatusData
-	// const data = ...
-
-	// TODO: Create an array of columns using projectStatusData
-	// const columns = ...
-
-	const data = [
-		{
-			unit: 'Centralized Services & Digital Branches',
-			pem: '10001',
-			title: 'Lifemoments',
-			desc: 'test2',
-			supervisor: 'Super3',
-			team: [ 'Member1', 'Member2', 'Member3' ],
-			startingDate: chooseDaysBefore(30),
-			endingDate: chooseDaysAfter(10),
-			comments: 'many experienced developers in team',
-			mandays: 140,
-			lastUpdate: chooseDaysBefore(0),
-			status: 'On going',
-		},
+	const unit = [ 'Centralized Services & Digital Branches' ];
+	const pem = [ '10000', '10001', '10010', '10011' ];
+	const title = [ 'Lifemoments', 'PricingTool', 'LendingTool', 'WizardTool' ];
+	const desc = [ 'test', 'test2', 'test3' ];
+	const supervisor = [ 'Super1', 'Super2', 'Super3' ];
+	const team = [
+		[ 'Member1', 'Member2', 'Member3' ],
+		[ 'Member4', 'Member5', 'Member6', 'Member7' ],
+		[ 'Member8', 'Member9' ],
+		[ 'Member10' ],
 	];
+	const startingDate = [ chooseDaysBefore(10), chooseDaysBefore(32), chooseDaysBefore(15), chooseDaysBefore(45) ];
+	const endingDate = [ chooseDaysAfter(40), chooseDaysAfter(60), chooseDaysAfter(50), chooseDaysAfter(4) ];
+	const comments = [
+		'nothing to mention',
+		'low team capacity due to holiday season',
+		'many experienced developers in team',
+	];
+	const mandays = [ 100, 140, 150 ];
+	const lastUpdate = [ chooseDaysBefore(3), chooseDaysBefore(1), chooseDaysBefore(0), , chooseDaysBefore(6) ];
+	const status = [ 'Started', 'On going', 'Finishing' ];
 
-	const columns = [
-		{
-			title: 'UNIT',
-			dataIndex: 'unit',
-			key: 'unit',
-			render: (text) => <a href="javascript:;">{text}</a>,
-		},
-		{
-			title: 'PEM',
-			dataIndex: 'pem',
-			key: 'pem',
-		},
-		{
-			title: 'TITLE',
-			dataIndex: 'title',
-			key: 'title',
-		},
-		{
-			title: 'SUPERVISOR',
-			key: 'supervisor',
-			dataIndex: 'supervisor',
-		},
-		{
-			title: 'TEAM',
-			key: 'team',
-			dataIndex: 'team',
-			render: (tags) => (
+	const projectStatusData = () => ({
+		unit: unit[Math.floor(Math.random() * unit.length)],
+		pem: pem[Math.floor(Math.random() * pem.length)],
+		title: title[Math.floor(Math.random() * title.length)],
+		desc: desc[Math.floor(Math.random() * desc.length)],
+		supervisor: supervisor[Math.floor(Math.random() * supervisor.length)],
+		team: team[Math.floor(Math.random() * team.length)],
+		startingDate: startingDate[Math.floor(Math.random() * startingDate.length)],
+		endingDate: endingDate[Math.floor(Math.random() * endingDate.length)],
+		comments: comments[Math.floor(Math.random() * comments.length)],
+		mandays: mandays[Math.floor(Math.random() * mandays.length)],
+		lastUpdate: lastUpdate[Math.floor(Math.random() * lastUpdate.length)],
+		status: status[Math.floor(Math.random() * status.length)],
+	});
+
+	const data = [ ...Array(4).keys() ].map(() => projectStatusData());
+
+	const columns = Object.keys(projectStatusData()).map((key) => ({
+		title: key.toUpperCase(),
+		dataIndex: key,
+		key: key,
+		render: (inputElement) =>
+			key === 'unit' ? (
+				<a href="javascript:;">{inputElement}</a>
+			) : key === 'team' ? (
 				<span>
-					{tags.map((tag) => {
-						console.log('tags', tag);
-						return <Tag key={tag}>{tag}</Tag>;
+					{inputElement.map((tag) => {
+						return <Tag key={tag}>{tag.toUpperCase()}</Tag>;
 					})}
 				</span>
+			) : key === 'status' ? (
+				<span>
+					<Tag
+						color={
+							inputElement === 'Started' ? 'green' : inputElement === 'On going' ? 'geekblue' : 'volcano'
+						}
+						key={inputElement}
+					>
+						{inputElement.toUpperCase()}
+					</Tag>
+				</span>
+			) : (
+				inputElement
 			),
-		},
-		{
-			title: 'STARTINGDATE',
-			key: 'startingDate',
-			dataIndex: 'startingDate',
-		},
-		{
-			title: 'ENDINGDATE',
-			key: 'endingDate',
-			dataIndex: 'endingDate',
-		},
-		{
-			title: 'COMMENTS',
-			key: 'comments',
-			dataIndex: 'comments',
-		},
-		{
-			title: 'MANDAYS',
-			key: 'mandays',
-			dataIndex: 'mandays',
-		},
-		{
-			title: 'LASTUPDATE',
-			key: 'lastUpdate',
-			dataIndex: 'lastUpdate',
-		},
-		{
-			title: 'STATUS',
-			key: 'status',
-			dataIndex: 'status',
-			render: (tag) => (
-				<Tag color={'geekblue'} key={tag}>
-					{tag.toUpperCase()}
-				</Tag>
-			),
-		},
-	];
+	}));
 
-	return data && <Table columns={columns} dataSource={data} />;
+	const newProjectData = {
+		unit: 'Centralized Services & Digital Branches',
+		pem: '10100',
+		title: 'Financial Terminal',
+		desc: 'test4',
+		supervisor: 'Super2',
+		team: [ 'Member11', 'Member12', 'Member13' ],
+		startingDate: chooseDaysBefore(0),
+		endingDate: chooseDaysAfter(400),
+		comments: 'fingers crossed',
+		mandays: 900,
+		lastUpdate: chooseDaysBefore(0),
+		status: 'Started',
+	};
+	const newRow = object(name, newProjectData);
+
+	return <Table columns={columns} dataSource={[ ...data, newRow ]} />;
 }, { notes: 'A very simple Table component' });
